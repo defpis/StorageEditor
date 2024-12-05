94aa5f213c0b717c2fe81be0e8dc1ca0d584497f
wenjun.super <wenjun.super@bytedance.com>
2024-12-05 18:53:52 +0800

    fix

--- public/scripts/cookie.js
+++ public/scripts/cookie.js
@@ -1,7 +1,10 @@
 chrome.runtime.sendMessage({ action: "getCookies", cookies: document.cookie });
 
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.action === "setCookie") {
-    console.log("setCookie");
-  }
-});
+if (!window.__StorageEditor__) {
+  chrome.runtime.onMessage.addListener((message) => {
+    if (message.action === "setCookie") {
+      console.log("setCookie");
+    }
+  });
+  window.__StorageEditor__ = true;
+}
